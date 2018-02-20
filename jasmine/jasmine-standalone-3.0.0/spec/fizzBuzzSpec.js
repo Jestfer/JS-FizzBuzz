cdf675f45f96fc69b50942956e32d83ddf5d54c3
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-20 18:07:06 +0000

    refactored and cleaned up code. Next: try and add jasmine matches to say, for example, expect(sth).toBeArrayOfSize(num)

--- jasmine/jasmine-standalone-3.0.0/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-3.0.0/spec/fizzBuzzSpec.js
@@ -1,26 +1,3 @@
-// describe('FizzBuzz', function() {
-//   // it('Checks if there are 100 elems in the array', function() {
-//   //   var range = Array.from({ length: 101 }, (x, i) => i);
-//   //   range.shift();
-//   //
-//   //   expect(FizzBuzz.print(100)).toEqual(range.join(', '));
-//   // });
-
-//   it('changes numbers divisible by 3 to Fizz', function() {
-//     expect(FizzBuzz(3)).toEqual('1, 2, Fizz');
-//   });
-
-//   it('changes numbers divisible by 5 to Buzz', function() {
-//     expect(FizzBuzz(5)).toEqual('1, 2, Fizz, 4, Buzz');
-//   });
-
-//   it('changes numbers divisible by both 3 and 5 to FizzBuzz', function() {
-//     expect(FizzBuzz(15)).toEqual(
-//       '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz'
-//     );
-//   });
-// });
-
 describe('FizzBuzz', function() {
 
   var fizzbuzz;
--- jasmine/jasmine-standalone-3.0.0/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-3.0.0/src/fizzBuzz.js
@@ -1,21 +1,3 @@
-// var FizzBuzz = function(num) {
-//   var arr = [];
-
-//   for (var i = 1; i <= num; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//       arr.push('FizzBuzz');
-//     } else if (i % 3 === 0) {
-//       arr.push('Fizz');
-//     } else if (i % 5 === 0) {
-//       arr.push('Buzz');
-//     } else {
-//       arr.push(i);
-//     }
-//   }
-
-//   return arr.join(', ');
-// };
-
 function FizzBuzz() {
 };
 
